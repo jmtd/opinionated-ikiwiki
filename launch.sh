0dd1bb3e91503713a36e8f9e6144b56db0fd8f37
Jonathan Dowland <jon@dow.land>
2021-01-08 11:21:10 +0000

    Remove stale indexdb when re-cloning srcdir

--- launch.sh
+++ launch.sh
@@ -7,8 +7,12 @@ set -euo pipefail
 if ! GIT_DIR=./src/.git git show-ref >/dev/null 2>&1; then
     mv src/.ikiwiki dot-ikiwiki
     rm -rf src
-    git clone --shared conf/git/ikiwiki.git src
+    git clone --quiet --shared conf/git/ikiwiki.git src
     mv dot-ikiwiki src/.ikiwiki
+
+    # remove indexdb, which caches the stale commit refs.
+    # See <https://ikiwiki.info/bugs/ikiwiki_explodes_when_git_rewrites_history/>
+    rm -f src/.ikiwiki/indexdb
 
     # is this needed in other circumstances?
     ikiwiki --setup ~/conf/setup --rebuild --wrappers
